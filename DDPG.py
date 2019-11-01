66f9951ead1414aa66a126d287b0c0bd83cd6a33
saeed Ahmadian <38537212+saeedahmadian@users.noreply.github.com>
2019-10-31 22:30:58 -0400

    Add files via upload
    using Torch for ddpg reinforcement learning

---
+++ DDPG.py
@@ -0,0 +1,250 @@
+import math
+import random
+import gym
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+from torch.distributions import Normal
+import matplotlib.pyplot as plt
+from GridTest import Grid
+import pandapower.networks as pn
+
+use_cuda = torch.cuda.is_available()
+device = torch.device("cuda" if use_cuda else "cpu")
+
+
+class ReplayBuffer:
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.buffer = []
+        self.position = 0
+
+    def push(self, state, action, reward, next_state, done):
+        if len(self.buffer) < self.capacity:
+            self.buffer.append(None)
+        self.buffer[self.position] = (state, action, reward, next_state, done)
+        self.position = (self.position + 1) % self.capacity
+
+    def sample(self, batch_size):
+        batch = random.sample(self.buffer, batch_size)
+        state, action, reward, next_state, done = map(np.stack, zip(*batch))
+        return state, action, reward, next_state, done
+
+    def __len__(self):
+        return len(self.buffer)
+
+
+class NormalizedActions:
+    def __init__(self,upp=1,low=-1):
+        self.upp = upp
+        self.low = low
+
+    def _action(self, action):
+        low_bound = self.low
+        upper_bound = self.upp
+
+        action = low_bound + (action + 1.0) * 0.5 * (upper_bound - low_bound)
+        action = np.clip(action, low_bound, upper_bound)
+
+        return action
+
+    def _reverse_action(self, action):
+        low_bound = self.low
+        upper_bound = self.upp
+
+        action = 2 * (action - low_bound) / (upper_bound - low_bound) - 1
+        action = np.clip(action, low_bound, upper_bound)
+
+        return action
+
+
+class OUNoise(object):
+    def __init__(self, action_space, mu=0.0, theta=0.15, max_sigma=0.3, min_sigma=0.3, decay_period=100000):
+        self.mu = mu
+        self.theta = theta
+        self.sigma = max_sigma
+        self.max_sigma = max_sigma
+        self.min_sigma = min_sigma
+        self.decay_period = decay_period
+        self.action_dim = action_space
+        self.low = -1
+        self.high = 1
+        self.reset()
+
+    def reset(self):
+        self.state = np.ones(self.action_dim) * self.mu
+
+    def evolve_state(self):
+        x = self.state
+        dx = self.theta * (self.mu - x) + self.sigma * np.random.randn(self.action_dim)
+        self.state = x + dx
+        return self.state
+
+    def get_action(self, action, t=0):
+        ou_state = self.evolve_state()
+        self.sigma = self.max_sigma - (self.max_sigma - self.min_sigma) * min(1.0, t / self.decay_period)
+        return np.clip(action + ou_state, self.low, self.high)
+
+
+class ValueNetwork(nn.Module):
+    def __init__(self, num_inputs, num_actions, hidden_size, init_w=3e-3):
+        super(ValueNetwork, self).__init__()
+
+        self.linear1 = nn.Linear(num_inputs + num_actions, hidden_size)
+        self.linear2 = nn.Linear(hidden_size, hidden_size)
+        self.linear3 = nn.Linear(hidden_size, 1)
+
+        self.linear3.weight.data.uniform_(-init_w, init_w)
+        self.linear3.bias.data.uniform_(-init_w, init_w)
+
+    def forward(self, state, action):
+        x = torch.cat([state, action], 2)
+        x = F.relu(self.linear1(x))
+        x = F.relu(self.linear2(x))
+        x = self.linear3(x)
+        return x
+
+
+class PolicyNetwork(nn.Module):
+    def __init__(self, num_inputs, num_actions, hidden_size, init_w=3e-3):
+        super(PolicyNetwork, self).__init__()
+
+        self.linear1 = nn.Linear(num_inputs, hidden_size)
+        self.linear2 = nn.Linear(hidden_size, hidden_size)
+        self.linear3 = nn.Linear(hidden_size, num_actions)
+
+        self.linear3.weight.data.uniform_(-init_w, init_w)
+        self.linear3.bias.data.uniform_(-init_w, init_w)
+
+    def forward(self, state):
+        x = F.relu(self.linear1(state))
+        x = F.relu(self.linear2(x))
+        x = F.tanh(self.linear3(x))
+        return x
+
+    def get_action(self, state):
+        state = torch.FloatTensor(state).unsqueeze(0).to(device)
+        action = self.forward(state)
+        return action.detach().cpu().numpy()[0, 0]
+
+
+def ddpg_update(batch_size,value_net,policy_net,target_value_net,target_policy_net,
+                policy_optimizer,value_optimizer,
+                replay_buffer,
+                gamma=0.99,
+                min_value=-np.inf,
+                max_value=np.inf,
+                soft_tau=1e-2):
+    state, action, reward, next_state, done = replay_buffer.sample(batch_size)
+
+    state = torch.FloatTensor(state).to(device)
+    next_state = torch.FloatTensor(next_state).to(device)
+    action = torch.FloatTensor(action).to(device)
+    reward = torch.FloatTensor(reward).unsqueeze(1).to(device)
+    done = torch.FloatTensor(np.float32(done)).unsqueeze(1).to(device)
+
+    policy_loss = value_net(state, policy_net(state))
+    policy_loss = -policy_loss.mean()
+
+    next_action = target_policy_net(next_state)
+    target_value = target_value_net(next_state, next_action.detach())
+    expected_value = reward + (1.0 - done) * gamma * target_value
+    expected_value = torch.clamp(expected_value, min_value, max_value)
+    # ss = torch.reshape(state, [-1, 70])
+    value = value_net(state, action)
+    criterion=nn.MSELoss()
+    value_loss = criterion(value, expected_value.detach())
+
+    policy_optimizer.zero_grad()
+    policy_loss.backward()
+    policy_optimizer.step()
+
+    value_optimizer.zero_grad()
+    value_loss.backward()
+    value_optimizer.step()
+
+    for target_param, param in zip(target_value_net.parameters(), value_net.parameters()):
+        target_param.data.copy_(
+            target_param.data * (1.0 - soft_tau) + param.data * soft_tau
+        )
+
+    for target_param, param in zip(target_policy_net.parameters(), policy_net.parameters()):
+        target_param.data.copy_(
+            target_param.data * (1.0 - soft_tau) + param.data * soft_tau
+        )
+
+
+# env = NormalizedActions(gym.make("Pendulum-v0"))
+env = Grid(pn.case5())
+Ns=env.StateFeatures()[0]*env.StateFeatures()[1]
+state_dim = Ns
+action_dim = env.ActionFeature()
+ou_noise = OUNoise(action_dim)
+
+# state_dim = env.observation_space.shape[0]
+# action_dim = env.action_space.shape[0]
+hidden_dim = 256
+
+value_net = ValueNetwork(state_dim, action_dim, hidden_dim).to(device)
+policy_net = PolicyNetwork(state_dim, action_dim, hidden_dim).to(device)
+
+target_value_net = ValueNetwork(state_dim, action_dim, hidden_dim).to(device)
+target_policy_net = PolicyNetwork(state_dim, action_dim, hidden_dim).to(device)
+
+for target_param, param in zip(target_value_net.parameters(), value_net.parameters()):
+    target_param.data.copy_(param.data)
+
+for target_param, param in zip(target_policy_net.parameters(), policy_net.parameters()):
+    target_param.data.copy_(param.data)
+
+value_lr = 1e-3
+policy_lr = 1e-4
+
+value_optimizer = optim.Adam(value_net.parameters(), lr=value_lr)
+policy_optimizer = optim.Adam(policy_net.parameters(), lr=policy_lr)
+
+value_criterion = nn.MSELoss()
+
+replay_buffer_size = 1000000
+replay_buffer = ReplayBuffer(replay_buffer_size)
+
+max_frames = 24000
+max_steps = 20
+frame_idx = 0
+rewards = []
+batch_size = 32
+
+
+while frame_idx < max_frames:
+    env.reset()
+    state = env.InitState().reshape([-1, Ns])
+    ind = np.random.choice(np.arange(env.net.line.shape[0]))
+    env.Attack(ind)
+    ou_noise.reset()
+    episode_reward = 0
+
+    for step in range(max_steps):
+        action = policy_net.get_action(state)
+        action = ou_noise.get_action(action, step)
+        next_state, reward, done = env.take_action(action)
+        next_state= next_state.reshape([-1,Ns])
+        action= np.array(action).reshape([-1,action_dim])
+
+        replay_buffer.push(state, action, reward, next_state, done)
+        if len(replay_buffer) > batch_size:
+            ddpg_update(batch_size,value_net,policy_net,target_value_net,target_policy_net,
+                policy_optimizer,value_optimizer,replay_buffer)
+
+        state = next_state
+        episode_reward += reward
+        frame_idx += 1
+        if done:
+            break
+
+    if frame_idx % 10 == 0:
+        np.save('reward', rewards)
+        print('In Episode: {}, the reward function is {}'.format(frame_idx, episode_reward))
+
+    rewards.append(episode_reward)
